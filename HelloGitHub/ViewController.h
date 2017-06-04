f5cb7f856663ca4dd6828758f9f10a6b3be3cff8
Er Sanjay Morya <singh.avaneesh96@gmail.com>
2017-06-04 08:15:58 +0530

    ;.

--- HelloGitHub/ViewController.h
+++ HelloGitHub/ViewController.h
@@ -3,7 +3,7 @@
 //  HelloGitHub
 //
 //  Created by Er Sanjay Morya on 03/06/17.
-//  Copyright © 2017 Learning. All rights reserved.
+//  Copyright © 2017 L earning. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
