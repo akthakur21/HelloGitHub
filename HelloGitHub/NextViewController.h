2efef6b0abd7d1cc00f51aa61744552d28d90db5
Er Sanjay Morya <singh.avaneesh96@gmail.com>
2017-06-18 13:07:19 +0530

    Changes And Add new Files

---
+++ HelloGitHub/NextViewController.h
@@ -0,0 +1,13 @@
+//
+//  NextViewController.h
+//  HelloGitHub
+//
+//  Created by Er Sanjay Morya on 18/06/17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextViewController : UIViewController
+
+@end
---
+++ HelloGitHub/NextViewController.m
@@ -0,0 +1,37 @@
+//
+//  NextViewController.m
+//  HelloGitHub
+//
+//  Created by Er Sanjay Morya on 18/06/17.
+//  Copyright © 2017 Learning. All rights reserved.
+//
+
+#import "NextViewController.h"
+
+@interface NextViewController ()
+
+@end
+
+@implementation NextViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
